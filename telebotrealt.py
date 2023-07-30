ca24c77c8272f53a0bcb14c21af15d63dec233cf
SergeyPyt <virvich.serg.yandex.by>
2023-07-30 22:36:47 +0300

    2 files changed(realt.py, telebotrealt.py)

--- telebotrealt.py
+++ telebotrealt.py
@@ -1,22 +1,30 @@
+from constants import bot
+
 from realt import ParsRealt
 
-from constants import bot
+from constants import url
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    mess = f'Привет, {message.from_user.first_name}'
+    mess = f'Привет, {message.from_user.first_name}, Приступим к выбору квартиры. Напишите количество комнат в квартире.'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 @bot.message_handler()
-def apartment(message):
-    if message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Приступим к выбору квартиры')
-    elif message.text == '/help':
-        bot.send_message(message.chat.id, 'Начни с приветствия)')
-    else:
-        bot.send_message(message.chat.id, 'Я тебя не понимаю, напиши /help.')
+def number_of_rooms(message):
+    if message.text == "1":
+        bot.send_message(message.chat.id,
+                         f"{ParsRealt(url=url, values=1).parse(), ParsRealt.number_of_rooms(), ParsRealt.number_of_pages(), ParsRealt.apartment_links()}")
+    elif message.text == "2":
+        bot.send_message(message.chat.id,
+                         f"{ParsRealt(url=url, values=2).parse(), ParsRealt.number_of_rooms(), ParsRealt.number_of_pages(), ParsRealt.apartment_links()}")
+    elif message.text == "3":
+        bot.send_message(message.chat.id,
+                         f"{ParsRealt(url=url, values=3).parse(), ParsRealt.number_of_rooms(), ParsRealt.number_of_pages(), ParsRealt.apartment_links()}")
+    elif message.text == "4":
+        bot.send_message(message.chat.id,
+                         f"{ParsRealt(url=url, values=4).parse(), ParsRealt.number_of_rooms(), ParsRealt.number_of_pages(), ParsRealt.apartment_links()}")
 
 
 bot.polling(none_stop=True, interval=0)
